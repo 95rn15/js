f4def0b84538e1b83ce56c92f017dbccce038a03
95rn15 <rajan84210@gmail.com>
2025-09-05 19:50:36 +0000

    datatype and variable

---
+++ basics/datatype.js
@@ -0,0 +1,26 @@
+"use static" //treat all code as newer version 
+
+/*
+types of datatypes
+ 
+number
+
+bigint
+
+string
+
+boolean
+
+null
+
+undefined 
+
+symbol 
+
+object
+
+*/
+
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
---
+++ basics/variable.js
@@ -0,0 +1,10 @@
+const symbolnumber="79012266"
+let name = "rajan sah"
+var age ="23"
+semesterpassed ="1,4"
+
+name ="rajan kumar sah"
+semesterpassed ="1,3,4"
+var passedyear=
+
+console.table([symbolnumber,name,age,semesterpassed,passedyear])
